34806e8a0a9137c37bac042aceac18f3ecaca1bd
unknown <Imagine@Gaspar.virtual.uniandes.edu.co>
2015-06-18 17:08:11 -0500

    Se agrega la UI para mostrar los numeros de intentos restantes, tambien el script que las controla.

---
+++ TimeLand/Assets/Scripts/IU/MostrarIntentos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MostrarIntentos : MonoBehaviour {
+
+	public GameObject imagenIntento1;
+	public GameObject imagenIntento2;
+	public int intentos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		actualizar();
+
+	}
+
+	void actualizar(){
+
+		intentos = GameObject.Find ("Vida").GetComponent<Vida>().intentos;
+
+		if(intentos==2){
+			Destroy (imagenIntento2);
+		}
+
+		if(intentos==1){
+			Destroy (imagenIntento1);
+		}
+
+	}
+
+}
